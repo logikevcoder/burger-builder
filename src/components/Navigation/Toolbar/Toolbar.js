fc0a9027688a66ad3f11b33bba2bdc57e134bc04
“Kevin <logikevcoder@gmail.com>
2018-11-16 16:45:30 -0600

    added logo with styling

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
       <button>Menu</button>
-      <div>LOGO</div>
+      <Logo />
       <nav>
         ...
       </nav>
